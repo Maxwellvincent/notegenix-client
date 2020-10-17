f77b1b1ace7d133e521c6734abd9b4558c1de54d
Louis M <louismaxwell@knights.ucf.edu>
2020-10-17 13:04:47 -0400

    updated Icons to give landing page more direction

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css'
 import HeroSection from '../HeroSection/HeroSection'
+import InfoSection from '../InfoSection/InfoSection';
 
 function Home (){
     return (
         <>
-        <HeroSection/>
+            <HeroSection/>
+            <InfoSection/>
         </>
     )
 }
